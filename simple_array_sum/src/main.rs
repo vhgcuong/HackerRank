ac939d103abb3b4b82c71a8b843a7caec7404112
Yotoko <cuong.vuhung@vti.com.vn>
2022-08-25 17:30:08 +0700

    ____

--- simple_array_sum/src/main.rs
+++ simple_array_sum/src/main.rs
@@ -2,10 +2,10 @@ fn main() {
     let elements = [1, 2, 3, 4, 10, 11];
 
     // Cach 1
-    let mut result = 0;
-    for element in elements {
-        result += element;
-    }
+    // let mut result = 0;
+    // for element in elements {
+    //     result += element;
+    // }
 
     // Cach 2
     let result: i32 = elements.iter().sum();
